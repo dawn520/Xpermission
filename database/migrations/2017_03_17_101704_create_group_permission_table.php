3e12830745b498e1fc21098ea01a918116d8328d
zhouchenxi <admin@zhouchenxi.cn>
2017-03-17 10:59:19 +0800

    fix bugs

---
+++ database/migrations/2017_03_17_101704_create_group_permission_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupPermissionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_permission', function (Blueprint $table) {
+            $table->integer('group_id')->unsigned();
+            $table->integer('permission_id')->unsigned();
+
+            $table->foreign('group_id')->references('id')->on('groups')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['group_id', 'permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('group_permission');
+    }
+}
